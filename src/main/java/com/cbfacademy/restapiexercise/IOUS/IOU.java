e197d5aec231e64654359568313c315090e3910e
Nyadej <naomiadejumo@gmail.com>
2024-08-01 19:55:37 +0100

    exercise 1 complete

---
+++ src/main/java/com/cbfacademy/restapiexercise/IOUS/IOU.java
@@ -0,0 +1,71 @@
+package com.cbfacademy.restapiexercise.IOUS;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ious")
+public class IOU {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String borrower;
+    private String lender; 
+    private BigDecimal amount;
+    private Instant dateTime;
+
+    public IOU() {
+    }
+
+    public IOU (String borrower, String lender, BigDecimal amount, Instant dateTime) {
+        this.borrower = borrower;
+        this.lender = lender;
+        this.amount = amount;
+        this.dateTime = dateTime;
+    }
+
+    public UUID getId () {
+        return id;
+    }
+    
+    public String getBorrower () {
+        return borrower;
+    }
+
+    public void setBorrower(String borrower) {
+        this.borrower = borrower;
+    }
+
+    public String getLender() {
+        return lender;
+    }
+
+    public void setLender(String lender) {
+        this.lender = lender;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BIgDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Instant getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Instant dateTime) {
+        this.dateTime = dateTime;
+    }
+}
