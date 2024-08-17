70f127a5284f0784972c3d3c890efecffdbfbbdb
Nyadej <naomiadejumo@gmail.com>
2024-08-17 21:30:32 +0100

    Created find borrower method in repository that accepts string borrower as parameter.

--- src/main/java/com/cbfacademy/restapiexercise/IOUS/IOURepository.java
+++ src/main/java/com/cbfacademy/restapiexercise/IOUS/IOURepository.java
@@ -1,9 +1,15 @@
 package com.cbfacademy.restapiexercise.IOUS;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Repository
 public interface IOURepository extends ListCrudRepository<IOU, UUID> {
     
+    List<IOU> findByBorrower(String borrower); // Returning a list of IOUs for each borrower
+
 }
