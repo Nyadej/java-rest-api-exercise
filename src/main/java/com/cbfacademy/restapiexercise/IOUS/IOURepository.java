41cfa6708ae579b0ac891a9d988237ba41837255
Nyadej <naomiadejumo@gmail.com>
2024-08-17 22:35:29 +0100

    removed RequestParam import from repository - incorrect placement

--- src/main/java/com/cbfacademy/restapiexercise/IOUS/IOURepository.java
+++ src/main/java/com/cbfacademy/restapiexercise/IOUS/IOURepository.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Repository
 public interface IOURepository extends ListCrudRepository<IOU, UUID> {
