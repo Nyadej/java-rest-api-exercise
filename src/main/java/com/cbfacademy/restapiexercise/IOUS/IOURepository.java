9ff0895bf4e79ab0463f5daac503fe8313134730
Nyadej <naomiadejumo@gmail.com>
2024-08-18 20:14:13 +0100

    Added repository method to get high value IOUs using native query

--- src/main/java/com/cbfacademy/restapiexercise/IOUS/IOURepository.java
+++ src/main/java/com/cbfacademy/restapiexercise/IOUS/IOURepository.java
@@ -3,6 +3,7 @@ package com.cbfacademy.restapiexercise.IOUS;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,8 @@ import org.springframework.stereotype.Repository;
 public interface IOURepository extends ListCrudRepository<IOU, UUID> {
     
     List<IOU> findByBorrower(String borrower); // Returning a list of IOUs for each borrower
+
+    @Query(value = "SELECT * FROM ious WHERE ious.value > (SELECT AVG(ious.value) FROM ious)", nativeQuery = true)
+    List<IOU> findHIghValueIOUs();
 
 }
